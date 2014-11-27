8211f7bfe9b9005c52964b1b677c9afe8912e958
Tesser Paolo <p.tesser921@gmail.com>
2014-11-27 15:45:55 +0100

    Desc: implementato un metodo di input. Definito nel package FileInputOutput. Manca implementazione di un metodo per l output.

--- src/FileIO/FileIO.java
+++ src/FileIO/FileIO.java
@@ -27,17 +27,41 @@ import java.util.ArrayList;
 
 public class FileIO {
 
-    private static Charset charset = StandardCharsets.UTF_8;
+    public static Charset charset = StandardCharsets.UTF_8;
 
     /**
      *
+     * @param inputPath percorso del file in input da leggere
+     * @return un ArrayList contenente un elemento per ogni riga del file passato
+     */
+    public static ArrayList<String> readContent(Path inputPath) {
+        ArrayList<String> content = new ArrayList<>(0);
+
+        try (BufferedReader reader = Files.newBufferedReader(inputPath, charset)) {
+            String line = null;
+            int i = 0;
+            while ((line = reader.readLine()) != null) {
+                content.add(line);
+                ++i;
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return content;
+    }
+
+
+
+    /**
+     * Esempio di main che legge un file in input dato e lo stampa
      * @param args rappresenta le stringhe da inserire da linea di comando per ricevere in input/output i file di testo
      */
+
     public static void main(String[] args) {
         // String inputFile = args[0];
         // String outputFile = args[1];
-        /*
-        String inputFile = "/Users/ptesser/Documents/repository/Bitbucket/progr_concorrente_distribuita/input_files/test2.txt"; // inserisco una percorso statico
+
+        String inputFile = "/Users/ptesser/Documents/repository/Bitbucket/progr_concorrente_distribuita/input_files/test1.txt"; // inserisco una percorso statico
         Path inputPath = Paths.get(inputFile);
         // Path outputPath = Paths.get(outputFile);
 
@@ -59,23 +83,6 @@ public class FileIO {
         // System.out.println(loader.getResource("FileIO/FileIO.class"));
 
     }
-
-    private static ArrayList<String> readContent(Path inputPath) {
-        ArrayList<String> content = new ArrayList<>(0);
-
-        try (BufferedReader reader = Files.newBufferedReader(inputPath, charset)) {
-            String line = null;
-            int i = 0;
-            while ((line = reader.readLine()) != null) {
-                content.add(line);
-                ++i;
-            }
-        } catch (IOException e) {
-            System.err.println(e);
-        }
-        return content;
-    }
-
 
 
 }
---
+++ src/FileInputOutput/FileIOMod1.java
@@ -0,0 +1,46 @@
+package FileInputOutput;
+import java.io.BufferedReader;
+// import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+/**
+ * FileIOMod1 Model Object
+ *
+ * <p>Classe che mi serve per gestire l'input e l'output da file</p>
+ * @author Tesser Paolo
+ * @version 0.1
+ */
+
+public class FileIOMod1 {
+
+    public static Charset charset = StandardCharsets.UTF_8;
+
+    /**
+     * @param inputPath percorso del file in input da leggere
+     * @return un ArrayList contenente un elemento per ogni riga del file passato
+     */
+    public static ArrayList<String> readContent(Path inputPath) {
+        ArrayList<String> content = new ArrayList<>(0);
+
+        try (BufferedReader reader = Files.newBufferedReader(inputPath, charset)) {
+            String line = null;
+            int i = 0;
+            while ((line = reader.readLine()) != null) {
+                content.add(line);
+                ++i;
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        return content;
+    }
+
+    // TO DO - implementazione del metodo writeContent
+
+}
