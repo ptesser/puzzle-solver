89574d595bf2af9b9e204b4ddf73f986b103ab63
Tesser Paolo <p.tesser921@gmail.com>
2014-11-27 16:30:01 +0100

    Desc: inserite le principali classi che servono per il progetto. Da terminare.

---
+++ src/PuzzleSolver/Puzzle/Puzzle.java
@@ -0,0 +1,10 @@
+package PuzzleSolver.Puzzle;
+
+/**
+ * @author Tesser Paolo
+ * @version 0.1
+ */
+public abstract class Puzzle { // scegliere se farla come interfaccia
+
+
+}
---
+++ src/PuzzleSolver/Puzzle/PuzzleCarattere.java
@@ -0,0 +1,9 @@
+package PuzzleSolver.Puzzle;
+
+/**
+ * @author Tesser Paolo
+ * @version 0.1
+ */
+public class PuzzleCarattere extends Puzzle { // se cambiata in interfaccia, modificare in implements
+
+}
