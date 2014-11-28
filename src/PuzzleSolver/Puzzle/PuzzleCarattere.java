cc58f7af7e9a6b8cdb675a0a7d60e42645a2b71d
Tesser Paolo <p.tesser921@gmail.com>
2014-11-28 11:17:07 +0100

    Desc: iniziata stesura dei campiti dati e della classe interna.

--- src/PuzzleSolver/Puzzle/PuzzleCarattere.java
+++ src/PuzzleSolver/Puzzle/PuzzleCarattere.java
@@ -1,4 +1,5 @@
 package PuzzleSolver.Puzzle;
+import java.util.ArrayList;
 
 /**
  * @author Tesser Paolo
@@ -8,7 +9,32 @@ public class PuzzleCarattere extends Puzzle { // se cambiata in interfaccia, mod
 
     public class PezzoCarattere extends Pezzo{
 
+        private char carattere;
+
+        /**
+         * <p>Costruttore della classe interna PezzoCarattere</p>
+         * @param id identificatore univoco del pezzo che stiamo considerando
+         * @param idNord identificatore univoco del pezzo a nord di quello che stiamo considerando
+         * @param idSud identificatore univoco del pezzo a sud di quello che stiamo considerando
+         * @param idEst identificatore univoco del pezzo a est di quello che stiamo considerando
+         * @param idOvest identificatore univoco del pezzo a ovest di quello che stiamo considerando
+         * @param c carattere del pezzo che stiamo considerando
+         */
+        public PezzoCarattere(String id, String idNord, String idSud, String idEst, String idOvest, char c){
+            super(id, idNord, idSud, idEst, idOvest);
+            this.carattere = c;
+        }
+
+        /**
+         * @return il carattere memorizzato nel pezzo
+         */
+        public char getCarattere(){ return this.carattere; }
 
     }
+
+    private ArrayList<PezzoCarattere> puzzle;
+
+
+
 
 }
