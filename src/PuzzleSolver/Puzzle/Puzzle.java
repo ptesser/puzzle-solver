1d5c916ad403d12ab22133f2e197b4ef8ec4be81
Tesser Paolo <p.tesser921@gmail.com>
2014-12-02 12:39:02 +0100

    Title: scritti metodi base. Desc: introdotti i metodi base della classe astratta Puzzle. Tutti i metodi per il momento sono astratti e ne viene fatto l'overriding nel classe PuzzleCarattere.
    Task: Non usato con Asana

--- src/PuzzleSolver/Puzzle/Puzzle.java
+++ src/PuzzleSolver/Puzzle/Puzzle.java
@@ -1,10 +1,14 @@
 package PuzzleSolver.Puzzle;
 
+import java.util.ArrayList;
+
 /**
  * @author Tesser Paolo
  * @version 0.1
  */
 public abstract class Puzzle { // scegliere se farla come interfaccia
 
-
+    public abstract ArrayList<Pezzo> getPuzzleElement();
+    public abstract int getNumCol();
+    public abstract int getNumRow();
 }
