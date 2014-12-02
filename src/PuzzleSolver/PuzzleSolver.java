282a4951f363382f495347d38f1cf8c7fe32386b
Tesser Paolo <p.tesser921@gmail.com>
2014-12-02 23:22:14 +0100

    Title: interrogati nel main i nuovi metodi. Desc: sono state aggiunte delle chiamate e delle stampe per testare i nuovi metodi di Puzzle.
    Task: Reported in #id_task

--- src/PuzzleSolver/PuzzleSolver.java
+++ src/PuzzleSolver/PuzzleSolver.java
@@ -16,12 +16,14 @@ public class PuzzleSolver {
 
     public static void main( String[] args ){
         String inputFile = "/Users/ptesser/Documents/repository/Bitbucket/progr_concorrente_distribuita/input_files/test2.txt";
-
+        String outputFile = "/Users/ptesser/Documents/repository/Bitbucket/progr_concorrente_distribuita/output_files/empty.txt";
         // String inputFile = args[0];
         // String outputFile = args[1];
         // Path dei file di prova: /Users/ptesser/Documents/repository/Bitbucket/progr_concorrente_distribuita/input_files/test1.txt
 
         Path inputPath = Paths.get(inputFile);
+        Path outputPath = Paths.get(outputFile);
+
         ArrayList<String> inputContent = FileIOMod1.readContent(inputPath);
 
         Puzzle puzzle1 = new PuzzleCharacter(inputContent);
@@ -30,6 +32,18 @@ public class PuzzleSolver {
         System.out.println("Dim HashMap: " + puzzle1.getPuzzleElementToSolve().size());
         System.out.println("Dim matrix [][]: " + puzzle1.getPuzzleElementSolved().length * puzzle1.getPuzzleElementSolved()[0].length);
 
+        System.out.println("=====================");
+
         puzzle1.solvePuzzle();
+        puzzle1.showPuzzleTerminal();
+
+        System.out.println("=====================");
+
+        ArrayList<String> outputContent = puzzle1.convertToArrayList();
+        System.out.println("Dim ArrayList output: " + outputContent.size());
+
+        System.out.println("=====================");
+        FileIOMod1.writeContent(outputPath, outputContent, puzzle1.getNumRow(), puzzle1.getNumCol());
+
     }
 }
