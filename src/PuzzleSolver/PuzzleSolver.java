fde5309e4aaa1be26087520a090edb196f6ef2d5
Tesser Paolo <p.tesser921@gmail.com>
2014-12-03 10:46:22 +0100

    Title: pulizia del main del client PuzzleSolver Desc: rimosse stampe non più significative inserite inizialmente per testare lo svolgimento corretto.
    Task: Reported in #id_task

--- src/PuzzleSolver/PuzzleSolver.java
+++ src/PuzzleSolver/PuzzleSolver.java
@@ -15,35 +15,29 @@ import java.util.ArrayList;
 public class PuzzleSolver {
 
     public static void main( String[] args ){
+        /* acquisisco i percorsi dei file di input e output che mi servono sotto forma di stringhe */
         String inputFile = "/Users/ptesser/Documents/repository/Bitbucket/progr_concorrente_distribuita/input_files/test2.txt";
         String outputFile = "/Users/ptesser/Documents/repository/Bitbucket/progr_concorrente_distribuita/output_files/empty.txt";
         // String inputFile = args[0];
         // String outputFile = args[1];
         // Path dei file di prova: /Users/ptesser/Documents/repository/Bitbucket/progr_concorrente_distribuita/input_files/test1.txt
 
+        /* trasformo le stringhe in input dei percorsi del file in oggetti Path */
         Path inputPath = Paths.get(inputFile);
         Path outputPath = Paths.get(outputFile);
 
+        /* leggo il contenuto del file in input e lo trasformo nel formato dati che mi serve */
         ArrayList<String> inputContent = FileIOMod1.readContent(inputPath);
 
+        /* creo un nuovo puzzle da risolvere, generato dall'input acquisito in precedenza */
         Puzzle puzzle1 = new PuzzleCharacter(inputContent);
-        System.out.println("num rows: " + puzzle1.getNumRow());
-        System.out.println("num columns: " + puzzle1.getNumCol());
-        System.out.println("Dim HashMap: " + puzzle1.getPuzzleElementToSolve().size());
-        System.out.println("Dim matrix [][]: " + puzzle1.getPuzzleElementSolved().length * puzzle1.getPuzzleElementSolved()[0].length);
-
-        System.out.println("=====================");
-
+        /* setto l'algoritmo che voglio usare per risolvere il puzzle */
         puzzle1.setStrategy(new SolverAlgStrategy());
+        /* risolvo il puzzle */
         puzzle1.solvePuzzle();
-        puzzle1.showPuzzleTerminal();
 
-        System.out.println("=====================");
-
+        /* prelevo l'output risolto nel formato che mi serve per la stampa e stampo */
         ArrayList<String> outputContent = puzzle1.convertToArrayList();
-        System.out.println("Dim ArrayList output: " + outputContent.size());
-
-        System.out.println("=====================");
         FileIOMod1.writeContent(outputPath, outputContent, puzzle1.getNumRow(), puzzle1.getNumCol());
 
     }
