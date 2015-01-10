425d257be7e41b03441e6502e9285146cff03e8b
Tesser Paolo <p.tesser921@gmail.com>
2015-01-10 03:04:58 +0100

    Title: aggiunti commenti su ris. prima colonna Desc: Task: Reported in #id_task Option: [option][option] #quante se ne vuole END

--- src/solver/SolverAlgStrategy.java
+++ src/solver/SolverAlgStrategy.java
@@ -46,10 +46,10 @@ public class SolverAlgStrategy implements SolverStrategy {
 
             // riordino la colonna più a sinistra
             for (int j = 0; j < (lengthRow - 1); j++){
-                Tile nowTile = p.getPuzzleElementSolved()[j][0];
-                String idTileSud = nowTile.getIdSouth();
-                Tile newTileSud = p.getPuzzleElementToSolve().get(idTileSud);
-                p.setPuzzleElementSolved(j+1,0,newTileSud);
+                Tile nowTile = p.getPuzzleElementSolved()[j][0]; // prelevo il pezzo corrente
+                String idTileSud = nowTile.getIdSouth(); // mi ricavo il suo id sud
+                Tile newTileSud = p.getPuzzleElementToSolve().get(idTileSud); // prelevo il pezzo a sud dell'id corrente
+                p.setPuzzleElementSolved(j+1,0,newTileSud); // salvo il pezzo a sud nella prossima posizione dell'array
             }
 
             // riordino tutte le righe a partire dal primo elemento della colonna più a sinistra di ciascuna
