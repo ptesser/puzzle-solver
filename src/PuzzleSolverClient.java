5f9ba6b907de382a1b9a8cf983d7954cf7dd197b
Tesser Paolo <p.tesser921@gmail.com>
2015-01-27 16:37:33 +0100

    Title: [src] refactoring del client e server Desc: viene effettuata una rivisitazione del main del client e del server per renderli congrui alla specifica della terza parte, integrando quindi le funzionalità RMI per condividere e usare l'oggetto remoto.
    Task: Reported in #id_task
    Option: [option][option]
    END

--- src/PuzzleSolverClient.java
+++ src/PuzzleSolverClient.java
@@ -1,11 +1,10 @@
 import fileinputoutput.*;
-import myexception.IdEmptyExcpt;
-import myexception.NumFieldRowExcpt;
-import puzzle.*;
-import solver.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.rmi.ConnectException;
+import java.rmi.Naming;
 import java.util.ArrayList;
+import objrem.ObjRem;
 
 /**
  * @author Tesser Paolo
@@ -15,48 +14,49 @@ import java.util.ArrayList;
 public class PuzzleSolverClient {
 
     public static void main( String[] args ){
-        /* acquisisco i percorsi dei file di input e output che mi servono sotto forma di stringhe */
 
+        /* acquisisco i percorsi dei file di input e output che mi servono sotto forma di stringhe */
         String inputFile = args[0];
         String outputFile = args[1];
         String nameServer = args[2];
 
-        /* trasformo le stringhe in input dei percorsi del file in oggetti Path */
-        Path inputPath = Paths.get(inputFile);
-        Path outputPath = Paths.get(outputFile);
-
-        /* leggo il contenuto del file in input e lo trasformo nel formato dati che mi serve */
-        FileIO file = new FileIOMod1();
-        ArrayList<String> inputContent = file.readContent(inputPath);
-
         try {
-             /* creo un nuovo puzzle da risolvere, generato dall'input acquisito in precedenza */
-            Puzzle puzzle1 = new PuzzleCharacter(inputContent);
-            /* setto l'algoritmo che voglio usare per risolvere il puzzle */
-            puzzle1.setStrategy(new SolverParStrategy());
-            /* risolvo il puzzle */
-            puzzle1.solvePuzzle();
+            /* recupero il riferimento all'oggetto remoto */
+            ObjRem obj = (ObjRem) Naming.lookup(nameServer);
+            /* mi prelevo un identificativo del client (potrebbe essere un ip o qualcosa di univo) */
+            String idClient = obj.getNewIdClient();
+            System.out.println("Sono il: " + idClient);
+
+            /* trasformo le stringhe in input dei percorsi del file in oggetti Path */
+            Path inputPath = Paths.get(inputFile);
+            Path outputPath = Paths.get(outputFile);
+
+            /* leggo il contenuto del file in input e lo trasformo nel formato dati che mi serve */
+            FileIO file = new FileIOMod1();
+            ArrayList<String> inputContent = file.readContent(inputPath);
+
+            /* setto il puzzle che voglio risolvere nel server remoto */
+            obj.setClientPuzzle(idClient, inputContent);
+            /* lancio la risoluzione sul server del puzzle */
+            obj.solve(idClient, "parallelo");
+            /* lancio la conversione sul server del puzzle per ottenere il formato che mi serve per la stampa */
+            obj.convert(idClient);
 
             /* prelevo l'output risolto nel formato che mi serve per la stampa e stampo */
-
-            ArrayList<String> outputContent = puzzle1.convertToArrayList();
+            ArrayList<String> outputContent = obj.getOutput(idClient);
             if (file instanceof FileIOMod1) {
-                ((FileIOMod1) file).writeContent(outputPath, outputContent, puzzle1.getNumRow(), puzzle1.getNumCol());
+                ((FileIOMod1) file).writeContent(outputPath, outputContent, obj.getPuzzleRow(idClient), obj.getPuzzleCol(idClient));
             } else {
                 System.out.println("Impossibile scrivere il puzzle su file.");
             }
 
 
+        } catch (ConnectException e){
+            System.out.println("Problemi di connessione con il server");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch (NumFieldRowExcpt e){
-            System.out.println(e.getError());
-            System.out.println(e.getEffect());
-        }
-        catch (IdEmptyExcpt e){
-            System.out.println(e.getError());
-            System.out.println(e.getEffect());
 
-        }
 
     }
 }
--- src/PuzzleSolverServer.java
+++ src/PuzzleSolverServer.java
@@ -1,12 +1,24 @@
+import logger.Logger;
+import objrem.*;
+import java.rmi.Naming;
+
+
 /**
  * @author Tesser Paolo
  * @version 0.1
  */
 public class PuzzleSolverServer {
 
-    public static void main (String[] args){
+    public static void main (String[] args) throws Exception{
+        /* prelevo il nome del server passato come input */
         String nameServer = args[0];
 
+        /* creo l'oggetto che andrò a rendere disponibile in remoto */
+        ObjRemImpl obj = new ObjRemImpl();
+
+        /* rendo disponibile l'oggetto ai client che si connettono sullo stesso nominativo */
+        Naming.rebind(nameServer,obj);
+        Logger.logger.info("SERVER PRONTO");
 
     }
 
