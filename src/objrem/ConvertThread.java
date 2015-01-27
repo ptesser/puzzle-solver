03337ea55d1c4b1f554d58242bcddf64ba2bd289
Tesser Paolo <p.tesser921@gmail.com>
2015-01-27 16:36:30 +0100

    Title: [src] new package per oggetto remoto Desc: viene creato un apposito package per contenere i costrutti che servono sia al client che al server per lavorare.
    Task: Reported in #id_task
    Option: [option][option]
    END

--- src/objrem/ConvertThread.java
+++ src/objrem/ConvertThread.java
@@ -1,8 +1,32 @@
 package objrem;
 
+import puzzle.Puzzle;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * @author Tesser Paolo
  * @version 0.1
  */
-public class ConvertThread {
+
+public class ConvertThread extends Thread{
+
+    private String idClient;
+    private Puzzle p;
+    private HashMap<String, ArrayList<String>> output;
+
+    /**
+     * @param idClient identificativo del client sul quale voglio inserire l'output della conversione
+     * @param p puzzle da convertire nel formato necessario alla scrittura su file
+     * @param out HashMap dove immagazzinare l'output della conversione a secondo del client
+     */
+    public ConvertThread(String idClient, Puzzle p, HashMap<String, ArrayList<String>> out){
+        this.idClient = idClient;
+        this.p = p;
+        this.output = out;
+    }
+
+    public void run(){
+        this.output.put(idClient, p.convertToArrayList());
+    }
 }
--- src/objrem/SolveThread.java
+++ src/objrem/SolveThread.java
@@ -1,8 +1,25 @@
 package objrem;
 
+import puzzle.Puzzle;
+
 /**
  * @author Tesser Paolo
  * @version 0.1
  */
-public class SolveThread {
+
+public class SolveThread extends Thread{
+    private Puzzle p;
+
+    /**
+     *
+     * @param p puzzle da risolvere
+     */
+    public SolveThread(Puzzle p){
+        this.p = p;
+    }
+
+    public void run(){
+        p.solvePuzzle();
+    }
+
 }
