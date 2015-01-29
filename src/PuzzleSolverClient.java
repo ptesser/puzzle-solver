dcb0bc94926b341de31a0ccfe9615ae685b074f7
Tesser Paolo <p.tesser921@gmail.com>
2015-01-29 14:39:33 +0100

    Title: [src] aggiunta exc e STEP per caduta server Desc: Task: Reported in #id_task Option: [option][option] END

--- src/PuzzleSolverClient.java
+++ src/PuzzleSolverClient.java
@@ -3,6 +3,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.rmi.ConnectException;
 import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.util.ArrayList;
 import objrem.ObjRem;
 
@@ -12,6 +13,8 @@ import objrem.ObjRem;
  */
 
 public class PuzzleSolverClient {
+
+    private static int STEP = 0;
 
     public static void main( String[] args ){
 
@@ -25,6 +28,8 @@ public class PuzzleSolverClient {
             ObjRem obj = (ObjRem) Naming.lookup(nameServer);
             /* mi prelevo un identificativo del client (potrebbe essere un ip o qualcosa di univo) */
             String idClient = obj.getNewIdClient();
+            STEP = 1; /* id prelevato in remoto */
+
             System.out.println("Sono il: " + idClient);
 
             /* trasformo le stringhe in input dei percorsi del file in oggetti Path */
@@ -34,27 +39,81 @@ public class PuzzleSolverClient {
             /* leggo il contenuto del file in input e lo trasformo nel formato dati che mi serve */
             FileIO file = new FileIOMod1();
             ArrayList<String> inputContent = file.readContent(inputPath);
+            STEP = 2; /* lettura del file in locale effettuata */
 
             /* setto il puzzle che voglio risolvere nel server remoto */
             obj.setClientPuzzle(idClient, inputContent);
+            STEP = 3; /* inviato puzzle da risolvere al server */
+
             /* lancio la risoluzione sul server del puzzle */
             obj.solve(idClient, "parallelo");
+
+
             /* lancio la conversione sul server del puzzle per ottenere il formato che mi serve per la stampa */
             obj.convert(idClient);
+            STEP = 4; /* risolto  puzzle sul server */
 
             /* prelevo l'output risolto nel formato che mi serve per la stampa e stampo */
             ArrayList<String> outputContent = obj.getOutput(idClient);
+            STEP = 5; /* convertito puzzle sul server  e ottenuto in locale */
+
+            int numRow = obj.getPuzzleRow(idClient);
+            int numCol = obj.getPuzzleCol(idClient);
+            STEP = 6; /* prelevate numero colonne e righe sul server  */
+
+            /* anche se il server cada, da qua in poi il client riesce a terminare */
             if (file instanceof FileIOMod1) {
-                ((FileIOMod1) file).writeContent(outputPath, outputContent, obj.getPuzzleRow(idClient), obj.getPuzzleCol(idClient));
+                ((FileIOMod1) file).writeContent(outputPath, outputContent, numRow, numCol);
             } else {
                 System.out.println("Impossibile scrivere il puzzle su file!");
             }
 
 
         } catch (ConnectException e){
-            System.out.println("Attenzione: problemi di connessione con il server!");
-            System.out.println("Riprovare in seguito!");
-        } catch (Exception e) {
+
+            switch (STEP){
+                case 0: {
+                    System.out.println("Attenzione: problemi di connessione con il server!");
+                    System.out.println("Riprovare in seguito!");
+                    break;
+                }
+                case 1: {
+                    System.out.println("Attenzione: problemi di connessione con il server!");
+                    System.out.println("Riprovare in seguito!");
+                    break;
+                }
+                case 2: {
+                    System.out.println("Attenzione: problemi di connessione con il server!");
+                    System.out.println("Lettura del file in input eseguita.");
+                    System.out.println("Riprovare in seguito a lanciare il programma!");
+                    break;
+                }
+                case 3: {
+                    System.out.println("Attenzione: problemi di connessione con il server!");
+                    System.out.println("Puzzle da risolvere inviato al server.");
+                    System.out.println("Riprovare in seguito a lanciare il programma!");
+                    break;
+                }
+                case 4: {
+                    System.out.println("Attenzione: problemi di connessione con il server!");
+                    System.out.println("Puzzle risolto sul server.");
+                    System.out.println("Riprovare in seguito a lanciare il programma!");
+                    break;
+                }
+                case 5: {
+                    System.out.println("Attenzione: problemi di connessione con il server!");
+                    System.out.println("Convertito puzzle e ottenuto in locale!");
+                    System.out.println("Riprovare in seguito a lanciare il programma!");
+                    break;
+                }
+            }
+
+        }
+        catch (NotBoundException e){
+            System.out.println("Attenzione: nome del server non trovato!");
+            System.out.println("Provare con il nome dato al server.");
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
 
